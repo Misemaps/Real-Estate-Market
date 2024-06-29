8c52893ad481a4fc692147e9a727449af560ce54
Misemaps <kihimam@gmail.com>
2024-06-29 17:24:28 +0300

    Add files via upload

---
+++ data/34BEDROOM_9.js
@@ -0,0 +1,1 @@
+var json_34BEDROOM_9 = {"type":"FeatureCollection","name":"34BEDROOM_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
---
+++ data/STUDIO_14.js
@@ -0,0 +1,1 @@
+var json_STUDIO_14 = {"type":"FeatureCollection","name":"STUDIO_14","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
